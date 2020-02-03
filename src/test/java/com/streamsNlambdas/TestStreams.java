716ba90a5b8e9f98317dee4e6974491cef52c12b
Fawad Malik <fawad.malik@hotmail.com>
2020-02-02 23:38:07 -0800

    Modifies test to add assert usage

--- src/test/java/com/streamsNlambdas/TestStreams.java
+++ src/test/java/com/streamsNlambdas/TestStreams.java
@@ -1,6 +1,5 @@
 package com.streamsNlambdas;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.Test;
@@ -9,51 +8,10 @@ import static org.hamcrest.Matchers.*;
 
 public class TestStreams {
 	@Test
-	public void whenIncrementSalaryForEachEmployee_thenApplyNewSalary() {
-		String poetry = "mary had a little lamb";
-		assertThat(poetry, allOf(
-				containsString("mary"),
-				containsString("lamb"),
-				startsWith("mary")
-				));
-		assertThat(poetry, anyOf(
-				containsString("mary"),
-				containsString("sally"),
-				startsWith("jerry"),
-				endsWith("lamb")
-				));
-		Animal[] animalsArray = new Animal[] {
-			    new Animal(AnimalType.SHEEP, "Black Sheep", 3), 
-			    new Animal(AnimalType.DOG, "Tramp", 1), 
-			    new Animal(AnimalType.DOG, "Lady", 1)
-		};
-		List<Animal> animals = Arrays.asList(animalsArray);
-		
-	    assertThat(animals, contains(
-	    	      hasProperty("type", equalTo(AnimalType.SHEEP))
-//	    	      hasProperty("type", equalTo(AnimalType.DOG)),
-//	    	      hasProperty("type", equalTo(AnimalType.CAT))
-	    	    ));
+	public void whenAdvancePriorityForEachJira_thenNoLowPriorityJira() {
 		List<JiraIssue> jiras = getAllJiraIssues();
 		jiras.stream().forEach(jira -> jira.advancePriority());
-	    assertThat(jiras, contains(
-	    	      hasProperty("priority", equalTo(Priority.BLOCKER))
-//	    	      hasProperty("priority", equalTo(Priority.HIGH)),
-//	    	      hasProperty("priority", equalTo(Priority.MEDIUM))
-	    	      ));
-
-		// (JiraIssue, Matcher<Iterable<? extends Object>>)
-//		jiras.stream().forEach(jira ->
-//		assertThat(jira, contains(
-//				hasProperty("batch", equalTo(1))
-//				))
-//		);
-		
-//				contains(hasProperty("priority", equals(Priority.BLOCKER.name())))
-//				hasItem(allOf(hasProperty("priority", is(Priority.BLOCKER.name())))
-//				);
-
-
+		assertThat(jiras, everyItem(hasProperty("priority", is(not(Priority.LOW)))));
 	}
 
 	private static List<JiraIssue> getAllJiraIssues() {
@@ -67,16 +25,5 @@ public class TestStreams {
 				new JiraIssue(Status.CLOSED, Priority.HIGH, "TA-700")
 		};
 		return Arrays.asList(jiraArray);
-		
-//		ArrayList<JiraIssue> issues = new ArrayList<>();
-//		issues.add(new JiraIssue(Status.OPEN, Priority.LOW, "TA-100"));
-//		issues.add(new JiraIssue(Status.CLOSED, Priority.HIGH, "TA-200"));
-//		issues.add(new JiraIssue(Status.OPEN, Priority.LOW, "TA-300"));
-//		issues.add(new JiraIssue(Status.OPEN, Priority.HIGH, "TA-400"));
-//		issues.add(new JiraIssue(Status.CLOSED, Priority.MEDIUM, "TA-500"));
-//		issues.add(new JiraIssue(Status.CLOSED, Priority.HIGH, "TA-600"));
-//		issues.add(new JiraIssue(Status.CLOSED, Priority.HIGH, "TA-700"));
-
-//		return issues;
 	}
 }
